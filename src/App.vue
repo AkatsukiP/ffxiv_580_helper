388b80c44c26412e22c2b0327434696b8ebfd5c6
Siha <shigure.siha@Outlook.com>
2022-03-24 16:22:52 +0800

    更新中文物品翻译

--- src/App.vue
+++ src/App.vue
@@ -64,12 +64,15 @@ export default defineComponent({
       newStuffIconPath:['./assets/MempisangLumber.png','./assets/LunarAdamantiteIngot.png','./assets/RhodiumIngot.png','./assets/AmynodonLeather.png','./assets/GoldenSilk.png'],
       oldStuffNameJP: ['インテグラル材', 'コンドライトインゴット', 'スタークォーツ', 'オピオタウロスレザー', 'カエアンビロード'],
       oldStuffNameEN: ['Integral Lumber', 'Chondrite Ingot', 'Star Quartz', 'Ophiotauros Leather', 'AR-Caean Velvet'],
+      oldStuffNameCN: ['完满木材', '球粒陨石锭', '星点石英', '蛇牛革', '迷彩绒'],
       oldStuffIconPath:['./assets/IntegralLumber.png', './assets/ChondriteIngot.png', './assets/StarQuartz.png', './assets/OphiotaurosLeather.png', './assets/ARCaeanVelvet.png', ],
       alkahestNameJP: ['剛力の幻水G6','眼力の幻水G6','活力の幻水G6','知力の幻水G6','心力の幻水G6'],
       alkahestNameEN: ['Grade 6 Strength Alkahest', 'Grade 6 Dexterity Alkahest', 'Grade 6 Vitality Alkahest', 'Grade 6 Intelligence Alkahest', 'Grade 6 Mind Alkahest'],
+      alkahestNameCN: ['6级刚力之幻水','6级巧力之幻水','6级耐力之幻水','6级智力之幻水','6级意力之幻水'],
       alkahestIconPath:['./assets/StrengthAlkahest.png', './assets/DexterityAlkahest.png', './assets/VitalityAlkahest.png', './assets/IntelligenceAlkahest.png', './assets/MindAlkahest.png'],
       aethersandNameJP: ['巨岩の霊砂', '巨樹の霊砂','巨海の霊砂'],
       aethersandNameEN: ['Endstone Aethersand', 'Endwood Aethersand', 'Endtide Aethersand'],
+      aethersandNameCN: ['巨岩灵砂', '巨树灵砂', '巨海灵砂'],
       aethersandIconPath: ['./assets/Endstone.png', './assets/Endwood.png', './assets/Endtide.png'],
       //Moonlight Aethersand
     }
@@ -279,7 +282,7 @@ export default defineComponent({
       this.jobData.fill(0)
     },
     changeLang(lang:string) {
-      if(lang === 'JP' || lang === 'EN') {
+      if(lang === 'JP' || lang === 'EN' || lang== 'CN') {
         this.langFlag = lang
       }
     },
@@ -367,8 +370,10 @@ export default defineComponent({
                     <svg v-if="langFlag=='EN'" class="w-5 h-5 inline-block float-right fill-green-600" viewBox="0 0 1025 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3147" width="200" height="200"><path d="M511.9455 958.712809c-247.065724 0-448.054313-200.987589-448.054313-448.060313 0-247.065724 200.987589-448.054313 448.054313-448.054313 247.072724 0 448.060313 200.987589 448.060313 448.054313C960.005813 757.72422 759.017224 958.712809 511.9455 958.712809L511.9455 958.712809zM511.9455 126.893372c-211.60762 0-383.758124 172.150504-383.758124 383.758124 0 211.58062 172.150504 383.765124 383.758124 383.765124 211.58062 0 383.765124-172.184504 383.765124-383.765124C895.709624 299.043876 723.52512 126.893372 511.9455 126.893372L511.9455 126.893372zM511.9455 126.893372" p-id="3148"></path><path d="M726.94813 391.825148c-12.545037-12.448036-32.837096-12.322036-45.249133 0.254001L448.388314 627.94784l-103.280303-106.122311c-12.350036-12.707037-32.612096-12.932038-45.249133-0.640002-12.678037 12.322036-12.965038 32.612096-0.640002 45.251133l126.032369 129.522379c0.064 0.093 0.190001 0.093 0.254001 0.190001 0.064 0.064 0.097 0.191001 0.161 0.254001 2.017006 1.988006 4.512013 3.204009 6.88102 4.547013 1.250004 0.674002 2.241007 1.793005 3.52001 2.305007 3.873011 1.601005 8.000023 2.398007 12.096035 2.398007 4.063012 0 8.131024-0.796002 11.969035-2.334007 1.250004-0.513002 2.208006-1.539005 3.39401-2.178006 2.398007-1.344004 4.898014-2.525007 6.94502-4.542013 0.063-0.064 0.098-0.196001 0.190001-0.259001 0.064-0.094 0.161-0.128 0.259001-0.191001l256.253751-259.041759C739.626167 424.499244 739.494166 404.242184 726.94813 391.825148L726.94813 391.825148zM726.94813 391.825148" p-id="3149"></path></svg>
                   </div>
                 </MenuItem>
-                <MenuItem v-slot="{ active }" disabled>
-                  <div @click="changeLang('CN')" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">中文（暂不支持）</div>
+                <MenuItem v-slot="{ active }">
+                  <div @click="changeLang('CN')" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">中文
+                    <svg v-if="langFlag=='CN'" class="w-5 h-5 inline-block float-right fill-green-600" viewBox="0 0 1025 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3147" width="200" height="200"><path d="M511.9455 958.712809c-247.065724 0-448.054313-200.987589-448.054313-448.060313 0-247.065724 200.987589-448.054313 448.054313-448.054313 247.072724 0 448.060313 200.987589 448.060313 448.054313C960.005813 757.72422 759.017224 958.712809 511.9455 958.712809L511.9455 958.712809zM511.9455 126.893372c-211.60762 0-383.758124 172.150504-383.758124 383.758124 0 211.58062 172.150504 383.765124 383.758124 383.765124 211.58062 0 383.765124-172.184504 383.765124-383.765124C895.709624 299.043876 723.52512 126.893372 511.9455 126.893372L511.9455 126.893372zM511.9455 126.893372" p-id="3148"></path><path d="M726.94813 391.825148c-12.545037-12.448036-32.837096-12.322036-45.249133 0.254001L448.388314 627.94784l-103.280303-106.122311c-12.350036-12.707037-32.612096-12.932038-45.249133-0.640002-12.678037 12.322036-12.965038 32.612096-0.640002 45.251133l126.032369 129.522379c0.064 0.093 0.190001 0.093 0.254001 0.190001 0.064 0.064 0.097 0.191001 0.161 0.254001 2.017006 1.988006 4.512013 3.204009 6.88102 4.547013 1.250004 0.674002 2.241007 1.793005 3.52001 2.305007 3.873011 1.601005 8.000023 2.398007 12.096035 2.398007 4.063012 0 8.131024-0.796002 11.969035-2.334007 1.250004-0.513002 2.208006-1.539005 3.39401-2.178006 2.398007-1.344004 4.898014-2.525007 6.94502-4.542013 0.063-0.064 0.098-0.196001 0.190001-0.259001 0.064-0.094 0.161-0.128 0.259001-0.191001l256.253751-259.041759C739.626167 424.499244 739.494166 404.242184 726.94813 391.825148L726.94813 391.825148zM726.94813 391.825148" p-id="3149"></path></svg>
+                  </div>
                 </MenuItem>
               </div>
               <div class="px-1 py-1">
@@ -470,12 +475,15 @@ export default defineComponent({
                 <td class="px-6 py-3 whitespace-nowrap">
                   <img :src="getImage(index,'newstuff')" alt="" class="itemIcon"> 
                 </td>
-                <td class="px-6 py-3 whitespace-nowrap" v-if="langFlag=='JP'">
+                <td class="px-6 py-3 whitespace-nowrap" v-if="langFlag=='JP' || langFlag=='CN'">
                   {{newStuffNameJP[index]}}
                 </td>
                 <td class="px-6 py-3 whitespace-nowrap" v-if="langFlag=='EN'">
                   {{newStuffNameEN[index]}}
                 </td>
+                <!-- <td class="px-6 py-3 whitespace-nowrap" v-if="langFlag=='CN'">
+                  {{newStuffNameCN[index]}}
+                </td> -->
                 <td class="px-6 py-3 whitespace-nowrap">
                   {{newStuff[index]}}
                 </td>
@@ -500,6 +508,9 @@ export default defineComponent({
                 </td>
                 <td class="px-6 py-3 whitespace-nowrap" v-if="langFlag=='EN'">
                   {{oldStuffNameEN[index]}}
+                </td>
+                <td class="px-6 py-3 whitespace-nowrap" v-if="langFlag=='CN'">
+                  {{oldStuffNameCN[index]}}
                 </td>
                 <td class="px-6 py-3 whitespace-nowrap">
                   {{oldStuff[index]}}
@@ -526,6 +537,9 @@ export default defineComponent({
                 <td class="px-6 py-3 whitespace-nowrap" v-if="langFlag=='EN'">
                   {{alkahestNameEN[index]}}
                 </td>
+                <td class="px-6 py-3 whitespace-nowrap" v-if="langFlag=='CN'">
+                  {{alkahestNameCN[index]}}
+                </td>
                 <td class="px-6 py-3 whitespace-nowrap">
                   {{alkahest[index]}}
                 </td>
@@ -550,6 +564,9 @@ export default defineComponent({
                 </td>
                 <td class="px-6 py-3 whitespace-nowrap" v-if="langFlag=='EN'">
                   {{aethersandNameEN[index]}}
+                </td>
+                <td class="px-6 py-3 whitespace-nowrap" v-if="langFlag=='CN'">
+                  {{aethersandNameCN[index]}}
                 </td>
                 <td class="px-6 py-3 whitespace-nowrap">
                   {{aethersand[index]}}
